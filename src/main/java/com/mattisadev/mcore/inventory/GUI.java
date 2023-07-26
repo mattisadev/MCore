81250d7dc0adc7a866f1747bcc268ae534d67a3b
Daniel Ohrin <xqiam@outlook.com>
2023-07-26 09:39:47 -0500

    Updated version

--- src/main/java/com/mattisadev/mcore/inventory/GUI.java
+++ src/main/java/com/mattisadev/mcore/inventory/GUI.java
@@ -2,12 +2,15 @@ package com.mattisadev.mcore.inventory;
 
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 
 public interface GUI extends InventoryHolder {
 
     void onInventoryClick(InventoryClickEvent paramInventoryClickEvent);
+
+    void onInventoryOpen(InventoryOpenEvent paramInventoryOpenEvent);
 
     void onInventoryClose(InventoryCloseEvent paramInventoryCloseEvent);
 
